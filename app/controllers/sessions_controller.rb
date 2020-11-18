e123f265a8d74b8656c53faaef6dd8cd21615061
Rkebede <rutht.kebede@gmail.com>
2020-11-17 18:16:37 -0600

    adds sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,11 @@ class  SessionsController < ApplicationController
 //create custom route
 
 def login
-  user = User.find_by(username:)
-  
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    render json: email
+  else 
+    render json: {error: "Please check your email or password."}
 end 
 
 
